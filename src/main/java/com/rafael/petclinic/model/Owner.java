1c53894798adaf7e646f470e3a7720c71f50f2b0
Rafael <rafaelssouzaads@gmail.com>
2019-08-29 21:58:00 -0300

    adding POJOs. Closes #2.

---
+++ src/main/java/com/rafael/petclinic/model/Owner.java
@@ -0,0 +1,11 @@
+package com.rafael.petclinic.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Owner extends Person {
+}
---
+++ src/main/java/com/rafael/petclinic/model/Pet.java
@@ -0,0 +1,17 @@
+package com.rafael.petclinic.model;
+
+import com.rafael.petclinic.model.type.PetType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+}
