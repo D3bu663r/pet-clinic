cb20fbe495d1e813c4117fad3c0b2685ac434da2
Rafael <rafaelssouzaads@gmail.com>
2019-09-03 20:59:09 -0300

    refactoring to common interfaces. Closes #19.

--- pet-clinic-data/src/main/java/com/rafael/petclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/rafael/petclinic/service/OwnerService.java
@@ -2,14 +2,6 @@ package com.rafael.petclinic.service;
 
 import com.rafael.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-    Set<Owner> findAll(Long id);
-
-    Owner findById(Long id);
-
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-
-    Owner save(Owner owner);
 }
