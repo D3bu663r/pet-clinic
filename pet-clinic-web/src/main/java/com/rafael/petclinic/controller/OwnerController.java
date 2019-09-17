fefe5f8da6581a48daf9dc5fd3739e6730749220
Rafael Souza <rafael.souza@luizalabs.com>
2019-09-17 13:08:05 -0300

    adding list all owners. Closes #26.

--- pet-clinic-web/src/main/java/com/rafael/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/rafael/petclinic/controller/OwnerController.java
@@ -1,14 +1,23 @@
 package com.rafael.petclinic.controller;
 
+import com.rafael.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("owners")
 public class OwnerController {
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"", "index", "index.html"})
-    public String listAll() {
+    public String listAll(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
