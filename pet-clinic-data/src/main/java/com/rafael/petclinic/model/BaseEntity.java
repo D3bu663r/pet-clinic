fd81c2bb2a0ba437715f8a096e472d9b5fb68586
Rafael Souza <rafael.souza@luizalabs.com>
2019-11-01 08:57:34 -0300

    alterando base entity

--- pet-clinic-data/src/main/java/com/rafael/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/rafael/petclinic/model/BaseEntity.java
@@ -4,11 +4,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
